daffc16a162e9f660888ec3a9dc16063cee89174
hoseindig <hosein_dig@yahoo.com>
2022-04-18 18:10:29 +0430

    basic create

---
+++ src/components/steps/mainStep.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>main step</h1>
+  <div class="stepBox">
+    <stepBoxVue v-for="i in 5" :key="i" :isActive="true" />
+  </div>
+</template>
+
+<script>
+import stepBoxVue from "./stepBox.vue";
+export default {
+  components: {
+    stepBoxVue,
+  },
+};
+</script>
+
+<style>
+.stepBox {
+  /* position: relative; */
+  display: flex;
+}
+</style>
