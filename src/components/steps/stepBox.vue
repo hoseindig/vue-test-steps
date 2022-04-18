93fb0be507548b76610754a7b0f27c89c1b81590
hoseindig <hosein_dig@yahoo.com>
2022-04-18 18:10:45 +0430

    basic create

---
+++ src/components/steps/stepBox.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="box">
+    <!-- <h1>step box</h1> -->
+    <div class="parent-box">
+      <div class="box-inner"></div>
+    </div>
+    <div class="dot-box">
+      <div class="dot-out">
+        <div class="dot-inner"></div>
+      </div>
+    </div>
+    <!-- <div class="line-box">----</div> -->
+    <div class="text-box">test</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isActive: {
+      type: Boolean,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$deactive_color: rgb(166, 166, 166);
+$active_color: rgb(0, 202, 88);
+
+$step_size: 125px;
+$innter_step_size: $step_size - 9;
+$dot_size: 20px;
+$boader_size: 5px;
+.box {
+  width: $step_size + 35;
+  height: $step_size + 135;
+  &:hover {
+    cursor: pointer;
+  }
+}
+.parent-box {
+  width: $step_size;
+  height: $step_size;
+  border: $boader_size solid $deactive_color;
+  border-radius: 50%;
+  //   padding: $boader_size;
+  .box-inner {
+    width: $innter_step_size;
+    height: $innter_step_size;
+    border: $boader_size solid #ffffff;
+    background-color: $deactive_color;
+    border-radius: 50%;
+  }
+  &:after {
+    content: "";
+    position: relative;
+    top: 21px;
+    width: 0;
+    height: 0;
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+    border-top: 20px solid $deactive_color;
+    clear: both;
+  }
+}
+.dot-box {
+  width: $step_size;
+  height: $step_size - 30;
+  display: flex;
+  justify-content: center;
+
+  .dot-out {
+    width: $dot_size;
+    height: $dot_size;
+    border: $boader_size solid $deactive_color;
+    background-color: white;
+    position: relative;
+    top: 50px;
+    border-radius: 50%;
+  }
+}
+.text-box {
+  width: $step_size;
+  //   height: $step_size;
+}
+.line-box {
+  width: $step_size;
+  height: $step_size;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import mainPageVue from "@/components/mainPage.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import mainStepVue from "../components/steps/mainStep.vue";
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: mainPageVue,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/step",
+    name: "step",
+    component: mainStepVue,
+  },
+  {
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
